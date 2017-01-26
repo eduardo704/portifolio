33ab54e5973886a33e324e8be84ee293c663d380
eduardo704 <eduardo704@gmail.com>
2017-01-26 10:33:25 -0200

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name portifolioApp
+ * @description
+ * # portifolioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('portifolioApp', []);
